47ba1b54b9475fa7edb4559f15832b4992ae5661
Jae-cisco <76939023+Jae-cisco@users.noreply.github.com>
2022-05-10 16:39:21 +0900

    Update versions.tf

--- examples/minimal/versions.tf
+++ examples/minimal/versions.tf
@@ -1,10 +1,19 @@
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     aci = {
-      source  = "CiscoDevNet/aci"
-      version = "~> 0.7.0"
+      source = "ciscodevnet/aci"
     }
   }
+}
+
+#configure provider with your cisco aci credentials.
+provider "aci" {
+  # cisco-aci user name
+  username = var.username
+  # cisco-aci password
+  password = var.password
+  # cisco-aci url
+  url      = var.url
+  insecure = true
+  #proxy_url = "https://proxy_server:proxy_port"
 }
